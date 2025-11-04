435ba7bd170bb4a840f71fe5052b0171348ab0fa
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2025-11-04 10:28:10 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,11 @@
+# Output IAM Policy ARN
 output "iam_policy_arn" {
   description = "IAM Policy ARN"
-  value       = aws_iam_policy.lambda_s3_policy.arn
+  value       = aws_iam_policy.lambda_policy.arn
 }
 
+# Output Lambda Function Name
 output "lambda_function_name" {
   description = "Lambda Function Name"
-  value       = aws_lambda_function.create_thumbnail_lambda_function.function_name
-}
-
-output "cloudwatch_log_group_arn" {
-  description = "CloudWatch Log Group ARN"
-  value       = aws_cloudwatch_log_group.create_thumbnail_lambda_function_cloudwatch.arn
+  value       = aws_lambda_function.image_resizer.function_name
 }
