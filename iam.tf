6591937cc9809062623ab16c7825d0b0d0b6a99f
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2025-11-06 19:57:09 +0530

    Update IAM policy and role for Lambda execution

--- iam.tf
+++ iam.tf
@@ -27,6 +27,37 @@ resource "aws_iam_policy" "lambda_policy" {
           "${aws_s3_bucket.resized_images.arn}/*"
         ]
       }
+# IAM Role that allows Lambda to be assumed by the Lambda service
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
     ]
   })
 }
+
+# Attach AWS managed basic execution policy for CloudWatch logging
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Attach your custom Lambda S3 access policy
+resource "aws_iam_role_policy_attachment" "lambda_s3_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+    ]
+  })
+}
+
