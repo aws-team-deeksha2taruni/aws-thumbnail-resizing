06ea696764c488e971d7dcf66e38a55ed53428a4
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2025-11-06 20:56:56 +0530

    Refactor S3 bucket resource definitions

--- s3.tf
+++ s3.tf
@@ -1,12 +1,13 @@
 # Source S3 bucket — where original images are uploaded
 resource "aws_s3_bucket" "original_images" {
   bucket = "thumbnail-generator-source-bucket123"
-  acl    = "private"
+  
 }
 
 # Destination S3 bucket — where resized thumbnails will be stored
 resource "aws_s3_bucket" "resized_images" {
   bucket = "thumbnail-generator-destination-bucket123"
-  acl    = "private"
+  
 }
+
 
