f515ff78693efef4f3b28afe52bbe8fe1dcd1086
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2025-11-06 16:13:31 +0530

    Modify IAM policy for S3 access permissions
    Updated IAM policy to include ListBucket permission for S3.

--- iam.tf
+++ iam.tf
@@ -1,23 +1,3 @@
-# IAM Role: Lambda Execution Role
-# Allows Lambda to assume role and access CloudWatch + S3
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_s3_image_resizer_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-# IAM Policy: Grants permissions to create logs and access S3 buckets
 resource "aws_iam_policy" "lambda_policy" {
   name = "lambda_s3_image_resizer_policy"
 
@@ -37,19 +17,16 @@ resource "aws_iam_policy" "lambda_policy" {
         Effect = "Allow"
         Action = [
           "s3:GetObject",
-          "s3:PutObject"
+          "s3:PutObject",
+          "s3:ListBucket"
         ]
         Resource = [
+          aws_s3_bucket.original_images.arn,
           "${aws_s3_bucket.original_images.arn}/*",
+          aws_s3_bucket.resized_images.arn,
           "${aws_s3_bucket.resized_images.arn}/*"
         ]
       }
     ]
   })
-}
-
-# Attach the IAM Policy to the IAM Role
-resource "aws_iam_role_policy_attachment" "lambda_role_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
 }
