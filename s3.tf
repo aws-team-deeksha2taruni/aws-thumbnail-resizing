b0c1a33970ca90a4b344e5216c9136eb55fb5114
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2025-11-10 15:24:08 +0530

    Update S3 bucket names for original and resized images

--- s3.tf
+++ s3.tf
@@ -1,11 +1,12 @@
 # Source S3 bucket — where original images are uploaded
 resource "aws_s3_bucket" "original_images" {
-  bucket = "thumbnail-generator-source-bucket1"
+  bucket = "thumbnail-generator-source-bucket123"
 
 }
 
 # Destination S3 bucket — where resized thumbnails will be stored
 resource "aws_s3_bucket" "resized_images" {
-  bucket = "thumbnail-generator-destination-bucket"
+  bucket = "thumbnail-generator-destination-bucket123"
 
 }
+
