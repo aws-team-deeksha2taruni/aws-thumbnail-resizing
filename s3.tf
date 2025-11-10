c3800f0a67ad9277299123dfd0ade06fdd05a907
Taruni Mamidipaka <104311908+mm6993@users.noreply.github.com>
2025-11-10 15:17:13 +0530

    Refactor S3 bucket resources in s3.tf
    Removed lifecycle blocks from S3 bucket resources.

--- s3.tf
+++ s3.tf
@@ -1,17 +1,11 @@
+# Source S3 bucket — where original images are uploaded
 resource "aws_s3_bucket" "original_images" {
   bucket = "thumbnail-generator-source-bucket1"
 
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [bucket]
-  }
 }
 
+# Destination S3 bucket — where resized thumbnails will be stored
 resource "aws_s3_bucket" "resized_images" {
   bucket = "thumbnail-generator-destination-bucket"
 
-  lifecycle {
-    prevent_destroy = true
-    ignore_changes  = [bucket]
-  }
 }
